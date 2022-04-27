eb8b564b767151318b5b635c463e583a49617438
ruixuan.wang <ruixuan.wang@elementwin.com>
2022-04-27 20:46:37 +0800

    test branch3

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ if __name__ == '__main__':
 # if new branch not modified, it will point to master's version too.
 
 # now modify test again, I predict the head will still point to the test, but newest version.
+
+# the above is right, 
+# if we just create a new branch, the head will point master and new branch both.
+# if we modify the new branch to new branch2, then head can only point new branch2
+# so, when I reset the version to new branch, the head again point to both and there is not symbol 'Head-> XXXX'
+
+# now, I predict if I modify the branch2 to branch3, then the head will point to branch3,
+# and, I predict if I reset the branch3 to branch2, then the head will only point to branch2, with symbol 'head->branch2'
+# Let's TRY!
