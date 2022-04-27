b901d526175926b672f459d290226554324b5065
ruixuan.wang <ruixuan.wang@elementwin.com>
2022-04-27 17:07:36 +0800

    first_commit

---
+++ Git_test/Git_python.py
@@ -0,0 +1,19 @@
+"""
+Author: 王睿轩
+DATE: 2021/11/2
+Time: 22:45
+"""
+
+
+def func():
+    pass
+
+
+class A(object):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+    print('hello git')
